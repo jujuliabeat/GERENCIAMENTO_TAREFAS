e9861e570ed6345cf4a5543d50a6e666377281b6
Júlia Beatriz <101215446+jujuliabeat@users.noreply.github.com>
2023-12-08 01:14:21 -0300

    Delete GERENCIAMENTO_TAREFAS directory

--- GERENCIAMENTO_TAREFAS/crud_tarefas_ajax/view/login/sair.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Página que verifica se o usuário está logado
-require_once(__DIR__ . "/../../controller/LoginController.php");
-
-$loginCont = new LoginController();
-$loginCont->deslogar();
-header("location: login.php");
